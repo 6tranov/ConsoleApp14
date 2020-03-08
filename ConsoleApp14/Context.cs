132db945ed8d9f21e6d27a9235ebb58d8126b3c9
6tranov <sixtranov@yahoo.co.jp>
2020-03-08 20:47:49 +0900

    Strategyパターン

---
+++ ConsoleApp14/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp14
+{
+    class Context
+    {
+        private Converter<int, string> _execute { get; set; }
+        public Context(IStrategy strategy)
+        {
+            this._execute = strategy.FB;
+        }
+        public string FB(int num)
+        {
+            return _execute(num);
+        }
+        public void ChangeStrategy(IStrategy strategy)
+        {
+            this._execute = strategy.FB;
+        }
+    }
+}
--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -5,6 +5,8 @@ namespace ConsoleApp14
 {
     class Program
     {
+        static Context context = new Context(new NumStrategy());
+
         static void Main(string[] args)
         {
             for (int i = 0; i < 100; i++)
@@ -16,16 +18,18 @@ namespace ConsoleApp14
         {
             bool b3 = num % 3 == 0;
             bool b5 = num % 5 == 0;
+            context.ChangeStrategy(d[(b3, b5)]);
             return d[(b3, b5)].FB(num);
         }
 
         static Dictionary<(bool, bool), IStrategy> d
-             = new Dictionary<(bool, bool), IStrategy>
-             {
+            = new Dictionary<(bool, bool), IStrategy>
+            {
                  { (true,true),new FizzBuzzStrategy()},
                  {(true,false),new FizzStrategy() },
                  {(false,true),new BuzzStrategy() },
                  {(false,false),new NumStrategy() }
-             };
+            };
+
     }
 }
